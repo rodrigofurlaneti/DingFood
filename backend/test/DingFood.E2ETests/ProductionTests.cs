2b8e6f3f836578a99da5c141b3de81496bef7680
Rodrigo Luiz Madeira Furlaneti <rodrigofurlaneti31@hotmail.com>
2026-09-12 16:16:54 -0300

    Added

--- backend/test/DingFood.E2ETests/ProductionTests.cs
+++ backend/test/DingFood.E2ETests/ProductionTests.cs
@@ -26,7 +26,7 @@ internal static class Settings
     {
         get
         {
-            var uri = new Uri(Environment.GetEnvironmentVariable("E2E_BASE_URL") ?? "http://9.205.156.87:84");
+            var uri = new Uri(Environment.GetEnvironmentVariable("E2E_BASE_URL") ?? "http://20.226.121.58:80");
             if (uri.Scheme is not ("http" or "https") || !string.IsNullOrEmpty(uri.UserInfo))
                 throw new InvalidOperationException("E2E_BASE_URL deve ser HTTP(S), sem credenciais na URL.");
             return uri;
